624200de0432601be710b42b87284628212d780a
phanduopncv-sys <phanduopncv@gmail.com>
2025-11-18 21:02:28 +0700

    Delete utils directory

--- utils/jwt.js
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config.js";
-
-export const sign = (data) => jwt.sign(data, JWT_SECRET);
-export const verify = (token) => jwt.verify(token, JWT_SECRET);
