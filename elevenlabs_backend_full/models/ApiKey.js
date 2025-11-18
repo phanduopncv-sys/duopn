70733539db66f67b6d4558b02ed2a67f2cf92c77
phanduopncv-sys <phanduopncv@gmail.com>
2025-11-18 20:37:10 +0700

    Delete elevenlabs_backend_full directory

--- elevenlabs_backend_full/models/ApiKey.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const apiKeySchema = new mongoose.Schema({
-  key: String,
-  active: { type: Boolean, default: true },
-  remaining: Number
-});
-
-export default mongoose.model("ApiKey", apiKeySchema);
--- elevenlabs_backend_full/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  role: { type: String, default: "user" }
-});
-
-export default mongoose.model("User", userSchema);
